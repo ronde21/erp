6ed36950113860fbe421e526dc74574ee94e48a9
EmreJM <emre.olgun@myy.haaga-helia.fi>
2018-02-14 15:28:08 +0200

    odoo asennus skripti
    versio 11

---
+++ odooinstallation.sh
@@ -0,0 +1,7 @@
+setxkbmap -layout fi
+sudo apt update && sudo apt upgrade
+sudo su
+wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
+echo "deb http://nightly.odoo.com/11.0/nightly/deb/ ./" >> /etc/apt/sources.list.d/odoo.list
+apt-get update && apt-get install odoo
+exit

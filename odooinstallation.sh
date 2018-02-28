d2d63ce67c57d6c7e04fc638fb22b29c71d7d6ce
ronde21 <a1700601@myy.haaga-helia.fi>
2018-02-28 16:16:03 +0200

    Update odooinstallation.sh

--- odooinstallation.sh
+++ odooinstallation.sh
@@ -1,6 +1,7 @@
 setxkbmap -layout fi
-sudo apt update && sudo apt upgrade
+sudo apt-get update && sudo apt-get upgrade
 sudo su
+apt-get install postgresql -y
 wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
 echo "deb http://nightly.odoo.com/11.0/nightly/deb/ ./" >> /etc/apt/sources.list.d/odoo.list
 apt-get update && apt-get install odoo

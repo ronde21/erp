c28dc3d0b6b6ab28724c2df2275736b3c1db7d13
ronde21 <a1700601@myy.haaga-helia.fi>
2018-03-02 09:32:47 +0000

    Update odooinstallation.sh

--- odooinstallation.sh
+++ odooinstallation.sh
@@ -4,5 +4,5 @@ sudo apt-get install postgresql -y
 sudo wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
 sudo echo "deb http://nightly.odoo.com/11.0/nightly/deb/ ./" >> /etc/apt/sources.list.d/odoo.list
 sudo apt-get update
-apt-get install odoo -y
+sudo apt-get install odoo -y
 exit
